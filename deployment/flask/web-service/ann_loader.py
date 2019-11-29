86dfb6c28a00b59d52f54db3242074a337f5fbdd
erodsan <31965887+erodsan@users.noreply.github.com>
2019-11-29 03:36:57 -0500

    Update ann_loader.py

--- deployment/flask/web-service/ann_loader.py
+++ deployment/flask/web-service/ann_loader.py
@@ -6,16 +6,10 @@ from sklearn.externals import joblib
 
 def cargarModelo():
 
-    FILENAME_MODEL_TO_LOAD = "model_tf_rn.h5"
-    FILENAME_SCALER_TO_LOAD = "stdScaler.save"
-    FILENAME_LABELENCODER_X1_TO_LOAD = "lab_enc_al.save"
-    FILENAME_LABELENCODER_X2_TO_LOAD = "lab_enc_ns.save"
-    FILENAME_LABELENCODER_X3_TO_LOAD = "lab_enc_ec.save"
-    FILENAME_LABELENCODER_X4_TO_LOAD = "lab_enc_ne.save"
-    FILENAME_LABELENCODER_X5_TO_LOAD = "lab_enc_vs.save"
-    FILENAME_LABELENCODER_X6_TO_LOAD = "lab_enc_mr.save"
-    FILENAME_LABELENCODER_X7_TO_LOAD = "lab_enc_tv.save"
-    MODEL_PATH = "../../../models/hotel"
+    FILENAME_MODEL_TO_LOAD = "model_cable_hoist_f.h5"
+    FILENAME_SCALER_TO_LOAD = "mmScaler_f.save"
+    FILENAME_LABELENCODER_X1_TO_LOAD = "lab_enc_marca_f.save"
+    MODEL_PATH = "../../../models/cables"
 
     # Cargar la RNA desde disco
     loaded_model = load_model(MODEL_PATH + "/" + FILENAME_MODEL_TO_LOAD)
@@ -24,12 +18,6 @@ def cargarModelo():
     # Cargar los parametros usados
     loaded_scaler = joblib.load(MODEL_PATH + "/" + FILENAME_SCALER_TO_LOAD)
     loaded_labelEncoderX1 = joblib.load(MODEL_PATH + "/" + FILENAME_LABELENCODER_X1_TO_LOAD)
-    loaded_labelEncoderX2 = joblib.load(MODEL_PATH + "/" + FILENAME_LABELENCODER_X2_TO_LOAD)
-    loaded_labelEncoderX3 = joblib.load(MODEL_PATH + "/" + FILENAME_LABELENCODER_X3_TO_LOAD)
-    loaded_labelEncoderX4 = joblib.load(MODEL_PATH + "/" + FILENAME_LABELENCODER_X4_TO_LOAD)
-    loaded_labelEncoderX5 = joblib.load(MODEL_PATH + "/" + FILENAME_LABELENCODER_X5_TO_LOAD)
-    loaded_labelEncoderX6 = joblib.load(MODEL_PATH + "/" + FILENAME_LABELENCODER_X6_TO_LOAD)
-    loaded_labelEncoderX7 = joblib.load(MODEL_PATH + "/" + FILENAME_LABELENCODER_X7_TO_LOAD)
 
     graph = tf.get_default_graph()
-    return loaded_model,loaded_scaler,loaded_labelEncoderX1,loaded_labelEncoderX2,loaded_labelEncoderX3,loaded_labelEncoderX4,loaded_labelEncoderX5,loaded_labelEncoderX6,loaded_labelEncoderX7,graph
+    return loaded_model,loaded_scaler,loaded_labelEncoderX1,graph
